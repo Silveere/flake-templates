6865bfb6b7897be0eb6aa1508b1807bec546b795
NullBite <me@nullbite.com>
2025-11-20 21:29:13 -0500

    empty: clean up unused bindings, add import-tree
    I want to keep the flake.nix as minimal as possible; the purpose of it
    should be to declare metadata like systems/inputs/module imports.

--- empty/flake.nix
+++ empty/flake.nix
@@ -1,27 +1,18 @@
 {
-  description = "empty devShell template with Hello World";
+  description = "empty general-purpose flake with Hello World devShell";
 
   inputs = {
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-parts,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} ({
-      inputs,
-      self,
-      lib,
-      ...
-    }: {
+  outputs = {nixpkgs, ...} @ inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} ({inputs, ...}: {
       imports = [
-        ./nix/devshell.nix
+        (inputs.import-tree ./nix)
       ];
 
-      systems = import inputs.systems;
+      inherit (inputs) systems;
     });
 }
